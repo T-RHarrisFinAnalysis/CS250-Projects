e8ebb41e35880742675c941a28c23e54b1652ed8
Tim Harris <harristimothy2186@gmail.com>
2026-06-25 19:50:08 -0600

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,14 @@
+
+/*
+    Developer: Tim Harris
+    Date: May 8, 2026
+    Purpose: Simple program to print 'Hello, World!' to the console in C++.
+*/
+
+include  // Include input/output stream library
+
+int main() {
+    // Output Hello, World! to the console
+    std::cout << "Hello, World!" << std::endl;
+    return 0; // Indicate that the program ended successfully
+}
